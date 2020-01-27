171dab2bc295f05c16e74a860301840811ec175d
ryananicholson <ryananicholson@gmail.com>
2020-01-27 18:32:15 +0100

    Add files via upload

---
+++ reality.php
@@ -0,0 +1,24 @@
+<?php 
+
+function command_exist($cmd) {
+    $return = shell_exec(sprintf("which %s", escapeshellarg($cmd)));
+    return !empty($return);
+}
+
+$OS = PHP_OS;
+
+if ($OS == "Darwin") {
+    $OS_NAME = "macOS";
+    $OS_VER = exec("sw_vers -productVersion");
+} elseif (command_exist("lsb_release")) {
+    $OS_NAME = exec("lsb_release -a 2>/dev/null | grep Distributor | awk '{print $3}'");
+    $OS_VER = exec("lsb_release -a 2>/dev/null | grep Release | awk '{print $2}'");
+} elseif (command_exist("rpm")) {
+    $OS_NAME = exec("rpm --query redhat-release-server");
+    $OS_VER = "";
+}
+
+echo "Operating System: " . $OS_NAME . " " . $OS_VER . "<br/>";
+echo "Web Server: " . $_SERVER['SERVER_SOFTWARE'] . "<br/>";
+echo "PHP Version: " . phpversion() . "<br/>";
+?>
