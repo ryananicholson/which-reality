7658eba64ab5ad493d046b7a450fbca46374a790
kconnors60 <32774467+kconnors60@users.noreply.github.com>
2021-10-19 17:18:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>Oh geez... What reality am I in?</title>
 </head>
 
-<!-- TODO: Remember to upgrade Azure subscription before trial ends. Company credit card is 1234-5678-1234-5678. -->
+
   
 <body>
 <h1> Server Information</h1>
