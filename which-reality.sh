cd21af0e567856f136644ce6e3f79c05af0c8307
ryananicholson <ryananicholson@gmail.com>
2022-01-15 10:37:14 -0500

    Azure no longer supports PHP 7.3

--- which-reality.sh
+++ which-reality.sh
@@ -11,7 +11,7 @@ if [[ $1 == "deploy" ]]; then
   echo -e "\033[32mCreating the ${YOUR_USER_ID}-plan App service plan...\033[0m"
   az appservice plan create --name ${YOUR_USER_ID}-plan --resource-group ${YOUR_USER_ID}-rg --sku B1 --is-linux >/dev/null
   echo -e "\033[32mCreating the ${YOUR_USER_ID}-app Web App...\033[0m"
-  az webapp create --resource-group ${YOUR_USER_ID}-rg --plan ${YOUR_USER_ID}-plan --name ${YOUR_USER_ID}-app --runtime "PHP|7.3" --deployment-local-git >/dev/null 2>/dev/null
+  az webapp create --resource-group ${YOUR_USER_ID}-rg --plan ${YOUR_USER_ID}-plan --name ${YOUR_USER_ID}-app --runtime "PHP|7.4" --deployment-local-git >/dev/null 2>/dev/null
   echo -e "\033[32mPushing which-reality code...\033[0m"
   if [[ $(git remote | grep azure) ]]; then git remote remove azure; fi
   git remote add azure https://${YOUR_USER_ID}@${YOUR_USER_ID}-app.scm.azurewebsites.net/${YOUR_USER_ID}-app.git >/dev/null
